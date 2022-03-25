3f8599137a327b35587559eced357dc0c493e5e6
alberme <albertm@alphaworks.tech>
2022-03-25 01:33:37 -0700

    refactor(ProjectCard): change bootstrap css to tailwind css

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,25 +1,8 @@
-import Card from 'react-bootstrap/Card';
-import styled from 'styled-components';
 import placeholder from '../assets/placeholder.png';
-
-const StyledCard = styled(Card)`
-  width: 18rem;
-  padding: 0;
-  margin: 1.5rem;
-  border-radius: 1rem;
-  box-shadow: 0 10px 20px rgb(0 0 0 / 19%), 0 6px 6px rgb(0 0 0 / 23%);
-  animation-duration: 0.75s;
-  animation-fill-mode: both;
-  animation-timing-function: ease-in-out;
-
-  &:hover {
-    animation-name: bounce;
-  }
-`
 
 export default function ProjectCard ({ title, description, link, img }) {
   return (
-    <div className='max-w-xs rounded-xl mx-4 my-4 overflow-hidden shadow-lg'>
+    <div className='max-w-xs rounded-xl mx-4 my-4 overflow-hidden shadow-lg transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-amber-100 duration-300'>
       <img
         className="w-full"
         alt={`${title} preview`}
