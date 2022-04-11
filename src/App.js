4cb775b4660b57cd3162fc5c5ab70c8ccf09bccd
alberme <albertm@alphaworks.tech>
2022-04-11 01:36:42 -0700

    chore(app): begin tailwind overhaul

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import Home from './components/Home';
 import About from './components/About';
 import Footer from './components/Footer';
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import { Stack } from 'react-bootstrap';
+import { StackLayout, StackMain } from './components/Stack';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <HashRouter>
-        <Stack className="vh-100" direction="vertical" gap={0}>
+        <StackLayout>
           <HeaderNav />
+          <StackMain>
             <Routes>
               <Route exact path="/" element={<Home />} />
               <Route exact path="/about" element={<About />} />
             </Routes>
+          </StackMain>
           <Footer />
-        </Stack>
+        </StackLayout>
       </HashRouter>
     </div>
   );
