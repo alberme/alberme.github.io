3d193d2f9a92051162f5643e7eae38a8ded5065b
alberme <albertm@alphaworks.tech>
2022-04-16 18:45:45 -0700

    refactor(stack): rename stack components

--- src/App.js
+++ src/App.js
@@ -4,23 +4,21 @@ import Home from './components/Home';
 import About from './components/About';
 import Footer from './components/Footer';
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import { StackLayout, StackMain } from './components/Stack';
+import { Layout } from './components/Stack';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <HashRouter>
-        <StackLayout>
+        <Layout>
           <HeaderNav />
-          <StackMain>
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route exact path="/about" element={<About />} />
-            </Routes>
-          </StackMain>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
           <Footer />
-        </StackLayout>
+        </Layout>
       </HashRouter>
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,14 @@
-import { StackSection } from "./Stack"
+import { Section } from "./Stack"
 import ProjectCard from "./ProjectCard"
 
 export default function Projects({ projects }) {
   return (
-    <StackSection row>
+    <Section row>
       {
         projects.map(([ title, description, link, img ], i) => (
           <ProjectCard key={i} title={title} description={description} link={link} img={img} />
         ))
       }
-    </StackSection>
+    </Section>
   )
 }
