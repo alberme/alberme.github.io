f50bae19a7ed4bcbf0e333938ccfc79b6bb066f8
alberme <albertm@alphaworks.tech>
2022-05-27 15:29:09 -0700

    fix(nav bounce): add debouncing

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './HeaderNav'; // rename to Navbar
 import Footer from './Footer';
@@ -5,9 +6,23 @@ import { Main } from './Stack';
 import { ThemeProvider } from './ThemeProvider';
 
 export default function Layout() {
+  const [scrollBarAtTop, setScrollBarAtTop] = useState(true);
+
+  useEffect(() => {
+    window.onscroll = () => {
+      setTimeout(() => {
+        setScrollBarAtTop(Math.ceil(window.scrollY) === 0);
+      }, 100);
+    };
+
+    return () => {
+      window.onscroll = null
+    };
+  }, []);
+  
   return (
     <ThemeProvider>
-      <Header />
+      <Header shrink={!scrollBarAtTop}/>
       <Main>
         <Outlet />
       </Main>
