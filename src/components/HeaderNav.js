964eb6a061736e14c1f8b4eb0fc77b6f01f5490f
alberme <albertm@alphaworks.tech>
2022-05-27 15:30:54 -0700

    refactor(styles): change from bootstrap to tailwind

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -1,40 +1,75 @@
-import { useState } from 'react';
+import tw from 'tailwind-styled-components';
+import { FaEllipsisV } from 'react-icons/fa';
+// import { useState } from 'react';
 import { Link } from "react-router-dom"
-import { Navbar as RBNavbar, Nav as RBNav, Container, Offcanvas } from 'react-bootstrap';
+// import { Navbar as RBNavbar, Nav as RBNav, Container, Offcanvas } from 'react-bootstrap';
 import ThemeToggle from './ThemeToggle';
 
-const HeaderNav = () => {
-  const [show, setShow] = useState(false);
-  const handleOpen = () => setShow(true);
-  const handleClose = () => setShow(false);
+const Nav = tw.nav`
+  flex
+  justify-between
+  items-baseline
+  h-18
+  sticky
+  top-0
+  z-50
+  ${p => p.$shrink ? "py-1" : "py-3"}
+  px-10
+  backdrop-blur-md
+  backdrop-saturate-150
+  transition-all
+`
 
+const MenuButton = tw.button`
+  ${p => p.$shrink ? "text-lg" : "text-2xl"}
+  bg-transparent
+  text-white
+  cursor-pointer
+`
+
+const HeaderNav = ({ shrink }) => {
+  // const [show, setShow] = useState(false);
+  // const handleOpen = () => setShow(true);
+  // const handleClose = () => setShow(false);
+  console.log(shrink);
   return (
-    <RBNavbar bg="light" expand={false} sticky='top' style={{background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(255,135,70,1) 0%, rgba(130,234,255,1) 100%)"}}>
-      <Container fluid>
+    <Nav $shrink={shrink}>
+      <Link to="/">
+        <h2 className={shrink ? 'text-xl transition-all' : 'transition-all'}>Albert M</h2>
+      </Link>
+      <div className="flex w-28 justify-around px-3">
+        <ThemeToggle shrink={shrink} />
+        <MenuButton $shrink={shrink}>
+          <FaEllipsisV />
+        </MenuButton>
+      </div>
+    </Nav> 
+    // <RBNavbar bg="light" expand={false} sticky='top' style={{background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(255,135,70,1) 0%, rgba(130,234,255,1) 100%)"}}>
+    //   <Container fluid>
 
-        <RBNavbar.Brand as={Link} to="/"><h4>Albert M</h4></RBNavbar.Brand>
-        <ThemeToggle />
-        <RBNavbar.Toggle aria-controls="offcanvasNavbar" onClick={handleOpen} />
-        <RBNavbar.Offcanvas
-          id="offcanvasNavbar"
-          aria-labelledby="offcanvasNavbarLabel"
-          // placement="end"
-          show={show}
-          onHide={handleClose}
-          style={{backgroundColor: "whitesmoke"}}
-        >
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title id="offcanvasNavbarLabel">Portfolio</Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <RBNav className="justify-content-end flex-grow-1 pe-3">
-              <RBNav.Link as={Link} to="/" onClick={handleClose}>Home</RBNav.Link>
-              <RBNav.Link as={Link} to="/about" onClick={handleClose}>About Albert</RBNav.Link>
-            </RBNav>
-          </Offcanvas.Body>
-        </RBNavbar.Offcanvas>
-      </Container>
-    </RBNavbar>
+    //     <RBNavbar.Brand as={Link} to="/"><h4>Albert M</h4></RBNavbar.Brand>
+    //     <ThemeToggle />
+    //     <RBNavbar.Toggle aria-controls="offcanvasNavbar" onClick={handleOpen} />
+    //     <RBNavbar.Offcanvas
+    //       id="offcanvasNavbar"
+    //       aria-labelledby="offcanvasNavbarLabel"
+    //       // placement="end"
+    //       show={show}
+    //       onHide={handleClose}
+    //       style={{backgroundColor: "whitesmoke"}}
+    //     >
+    //       <Offcanvas.Header closeButton>
+    //         <Offcanvas.Title id="offcanvasNavbarLabel">Portfolio</Offcanvas.Title>
+    //       </Offcanvas.Header>
+    //       <Offcanvas.Body>
+    //         <RBNav className="justify-content-end flex-grow-1 pe-3">
+    //           <RBNav.Link as={Link} to="/" onClick={handleClose}>Home</RBNav.Link>
+    //           <RBNav.Link as={Link} to="/about" onClick={handleClose}>About Albert</RBNav.Link>
+    //         </RBNav>
+    //       </Offcanvas.Body>
+    //     </RBNavbar.Offcanvas>
+    //   </Container>
+    // </RBNavbar>
   )
 }
  
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,17 +1,17 @@
 import tw from 'tailwind-styled-components';
-
 import placeholder from '../assets/placeholder.png';
+import { FaGithub } from 'react-icons/fa';
 
 const Card = tw.div`
   flex
   flex-col
-  justify-between  
+  justify-between
   mx-4
   my-4
   max-w-[18rem]
   min-h-[24rem]
   md:max-w-[20rem]
-  md:min-h-[26rem]
+  md:min-h-[28rem]
   rounded-xl
   overflow-hidden
   shadow-lg
@@ -29,6 +29,18 @@ const Pill = tw.div`
   py-2
   rounded-full
   bg-red-300
+`
+
+const CardImage = tw.img`
+  w-full
+  h-[200px]
+  md:h-[220px]
+`
+
+const CardContentContainer = tw.div`
+  px-4
+  py-2
+  my-1
 `
 
 const getPillColor = (type) => {
@@ -49,16 +61,20 @@ const getPillColor = (type) => {
 export default function ProjectCard ({ title, description, link, img }) {
   return (
     <Card>
-      <img
-        className="w-full"
-        alt={`${title} preview`}
-        src={img || placeholder}
-      />
-      <div className="px-6 py-4">
-        <h4 className="font-bold text-xl mb-2">{title}</h4>
-        <p className="text-base">{description}</p>
-        <a className="dark:text-sky-200" target="_blank" rel="noreferrer" href={link}>Visit this project</a>
+      <div>
+        <CardImage
+          className="w-full"
+          alt={`${title} preview`}
+          src={img || placeholder}
+        />
+        <CardContentContainer>
+          <h4 className="font-bold text-xl mb-2">{title}</h4>
+          <p className="text-base">{description}</p>
+        </CardContentContainer>
       </div>
+      <CardContentContainer>
+        <a className="dark:text-sky-200" target="_blank" rel="noreferrer" href={link}>Visit this project</a>
+      </CardContentContainer>
     </Card>
   );
 }
